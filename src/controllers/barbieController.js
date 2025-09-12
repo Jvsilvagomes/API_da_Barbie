fb17f525178934218a921b2062927cddc9a03421
Jvsilvagomes <joao.silvassenai@gmail.com>
2025-09-12 11:45:47 -0300

    finaliza atividade barbie

--- src/controllers/barbieController.js
+++ src/controllers/barbieController.js
@@ -28,4 +28,60 @@ const getBarbieById = (req, res) => {
     })
 }
 
-export { getAllBarbies, getBarbieById};
+const createBarbie = (req, res) => {
+    const { nome, profissao, anoLancamento } = req.body;
+
+    if(!nome || !profissao){
+        return res.status(400).json({
+            success: false,
+            message: "nome e profissão são obriigatórios para uma barbie"
+        });
+    }
+
+    const novaBarbie = {
+        id: barbies.length + 1,
+        nome: nome,
+        profissao: profissao,
+        anoLancamento: parseInt(anoLancamento)
+    }
+
+    barbies.push(novaBarbie);
+
+    res.status(201).json({
+        success: true,
+        message: "Nova barbie criada",
+        barbie: novaBarbie
+    })
+}
+
+const deleteBarbie = (req,res) => {
+    const id = parseInt(req.params.id);
+
+    if(isNaN(id)){
+        return res.status(400).json({
+            success: false,
+            message: "O id deve ser válido"
+        })
+    }
+
+    const barbieRemover = barbies.find(b => b.id === id);
+
+    if(!barbieRemover) {
+        return res.status(404).json({
+            success: false,
+            message: `Barbie com o id ${id} não existe`
+        })
+    }
+
+    const barbiesFiltrados = barbies.filter(barbie => barbie.id !== id);
+
+    barbies.splice(0, barbies.length, ...barbiesFiltrados);
+
+    res.status(200).json({
+        success: true,
+        message: `a barbie ${id} foi removido com sucesso`
+    })
+
+}
+
+export { getAllBarbies, getBarbieById, createBarbie, deleteBarbie };
